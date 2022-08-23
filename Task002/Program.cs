b4ca0745ba1587a04dfef4efc9ae8bcb35d2f223
George <urban2014m@gmail.com>
2022-08-23 17:50:26 +0300

    Задача с ToInt32

---
+++ Task002/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу, которая на вход принимает число и выдаёт его квадрад.
+// (число умноженное на само себя).
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+System.Console.Write("Квадрат числа равен ");
+Console.WriteLine(result);
