36b05ae340739843d4fbfe70c885ed176c581949
George <urban2014m@gmail.com>
2022-08-29 17:51:23 +0300

    Принимает трёхзначное число и выдаёт последнюю цифру с проверкой

---
+++ Task006/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает последнюю цифру.
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    int digit = number % 10;
+    Console.WriteLine($"Последняя цифра числа {number} равна {digit}");
+}
+else
+{
+    System.Console.WriteLine("Введённое число не трёхзначное");
+}
